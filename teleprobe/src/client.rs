c022ccfa0dc9c3dea6c093b653ccebf34937a1a5
Dario Nieuwenhuis <dirbaio@dirbaio.net>
2023-05-30 00:12:26 +0200

    client: run binaries for different targets in parallel.

--- teleprobe/src/client.rs
+++ teleprobe/src/client.rs
@@ -1,16 +1,90 @@
-use anyhow::bail;
+use std::collections::HashMap;
+use std::fs::FileType;
+use std::path::PathBuf;
+
+use anyhow::{bail, Context};
+use futures::{stream, StreamExt};
+use object::{Object, ObjectSection};
+use reqwest::Client;
+use walkdir::WalkDir;
 
 use crate::api;
 
-pub async fn run(host: &str, token: &str, target: &str, elf: &str) -> anyhow::Result<()> {
-    let raw = std::fs::read(elf)?;
+#[derive(clap::Parser)]
+pub struct Command {
+    #[clap(flatten)]
+    credentials: Credentials,
 
-    println!("Trying to run {} on {}", elf, target);
-    let client = reqwest::Client::new();
+    #[clap(subcommand)]
+    cmd: Subcommand,
+}
+
+#[derive(clap::Parser)]
+struct Credentials {
+    #[clap(long, env = "TELEPROBE_TOKEN")]
+    token: String,
+
+    #[clap(long, env = "TELEPROBE_HOST")]
+    host: String,
+}
+
+#[derive(clap::Parser)]
+enum Subcommand {
+    ListTargets,
+    Run(RunCommand),
+}
+
+#[derive(clap::Parser)]
+pub struct RunCommand {
+    /// Teleprobe target to run the ELFs in.
+    /// If not specified, it will be autodetected based on the value of the `.teleprobe.target` section from the ELF.
+    #[clap(long)]
+    target: Option<String>,
+
+    /// ELF files to flash+run
+    files: Vec<String>,
+
+    /// Recursively run all files under the given directories
+    #[clap(short)]
+    recursive: bool,
+}
+
+pub async fn main(cmd: Command) -> anyhow::Result<()> {
+    if !cmd.credentials.host.starts_with("http") {
+        anyhow::bail!("Host must start with `http`.");
+    }
+
+    match cmd.cmd {
+        Subcommand::ListTargets => list_targets(&cmd.credentials).await,
+        Subcommand::Run(scmd) => run(&cmd.credentials, scmd).await,
+    }
+}
+
+fn detect_target(elf: &[u8]) -> anyhow::Result<String> {
+    let obj_file = object::File::parse(elf)?;
+    let Some(section) = obj_file.section_by_name(".teleprobe.target") else {
+        bail!(".teleprobe.target section not available")
+    };
+    let data = section.data()?;
+    if data.is_empty() {
+        bail!(".teleprobe.target section is empty")
+    }
+
+    Ok(String::from_utf8(data.to_vec()).context(".teleprobe.target contents are not a valid utf8 string.")?)
+}
+
+struct Job {
+    path: PathBuf,
+    target: String,
+    elf: Vec<u8>,
+}
+
+async fn run_job(client: &Client, creds: &Credentials, job: Job) -> anyhow::Result<()> {
+    println!("Trying to run {} on {}", job.path.display(), job.target);
     let res = client
-        .post(format!("{}/targets/{}/run", host, target))
-        .body(raw)
-        .bearer_auth(token)
+        .post(format!("{}/targets/{}/run", creds.host, job.target))
+        .body(job.elf)
+        .bearer_auth(&creds.token)
         .send()
         .await?;
 
@@ -19,7 +93,6 @@ pub async fn run(host: &str, token: &str, target: &str, elf: &str) -> anyhow::Re
         println!("Teleprobe response");
         println!("==================");
         println!("{}", res.text().await.unwrap_or_else(|_| "empty".to_string()));
-        Ok(())
     } else {
         println!("Error running the elf on the target device.status code");
         println!(
@@ -33,13 +106,65 @@ pub async fn run(host: &str, token: &str, target: &str, elf: &str) -> anyhow::Re
         );
         bail!("Running failed!");
     }
+    Ok(())
 }
 
-pub async fn list_targets(host: &str, token: &str) -> anyhow::Result<()> {
+async fn run(creds: &Credentials, cmd: RunCommand) -> anyhow::Result<()> {
+    let files = if cmd.recursive {
+        let mut files = Vec::new();
+
+        for f in cmd.files {
+            for entry in WalkDir::new(f).follow_links(true) {
+                let entry = entry?;
+                if entry.file_type().is_file() {
+                    files.push(entry.path().to_owned())
+                }
+            }
+        }
+
+        files
+    } else {
+        cmd.files.iter().map(|f| f.into()).collect()
+    };
+
+    let mut jobs_by_target: HashMap<String, Vec<Job>> = HashMap::new();
+
+    for path in files {
+        let elf: Vec<u8> = std::fs::read(&path)?;
+
+        let target = match cmd.target.clone() {
+            Some(t) => t,
+            None => detect_target(&elf)?,
+        };
+
+        jobs_by_target
+            .entry(target.clone())
+            .or_default()
+            .push(Job { path, target, elf });
+    }
+
+    let client = reqwest::Client::new();
+
+    stream::iter(jobs_by_target)
+        .flat_map_unordered(None, |(_, jobs)| {
+            let client = &client;
+            stream::iter(jobs)
+                .map(move |job| run_job(client, creds, job))
+                .buffer_unordered(2)
+        })
+        .for_each(|b| async move {
+            println!("{:?}", b);
+        })
+        .await;
+
+    Ok(())
+}
+
+async fn list_targets(creds: &Credentials) -> anyhow::Result<()> {
     let client = reqwest::Client::new();
     let res = client
-        .get(format!("{}/targets", host))
-        .bearer_auth(token)
+        .get(format!("{}/targets", creds.host))
+        .bearer_auth(&creds.token)
         .send()
         .await?;
 
--- teleprobe/src/main.rs
+++ teleprobe/src/main.rs
@@ -23,16 +23,7 @@ enum Cli {
         #[clap(long, default_value_t = 8080)]
         port: u16,
     },
-    Client {
-        #[clap(long, env = "TELEPROBE_TOKEN")]
-        token: String,
-
-        #[clap(long, env = "TELEPROBE_HOST")]
-        host: String,
-
-        #[clap(subcommand)]
-        command: ClientCommand,
-    },
+    Client(client::Command),
 }
 
 #[derive(clap::Subcommand)]
@@ -45,18 +36,6 @@ enum LocalCommand {
 
         #[clap(flatten)]
         probe: crate::probe::Opts,
-    },
-}
-
-#[derive(clap::Subcommand)]
-enum ClientCommand {
-    ListTargets,
-    Run {
-        #[clap(long)]
-        target: String,
-        /// ELF file to flash+run
-        #[clap(long)]
-        elf: String,
     },
 }
 
@@ -79,26 +58,16 @@ async fn async_main() -> anyhow::Result<()> {
 
     match cli {
         Cli::Local { command } => match command {
-            LocalCommand::ListProbes => crate::probe::list()?,
+            LocalCommand::ListProbes => crate::probe::list(),
             LocalCommand::Run { elf, probe } => {
                 let elf = std::fs::read(elf)?;
                 let mut sess = crate::probe::connect(probe)?;
 
                 let opts = crate::run::Options::default();
-                crate::run::run(&mut sess, &elf, opts)?
+                crate::run::run(&mut sess, &elf, opts)
             }
         },
-        Cli::Server { port } => crate::server::serve(port).await?,
-        Cli::Client { token, host, command } => {
-            if !host.starts_with("http") {
-                anyhow::bail!("Host must start with `http`.");
-            }
-            match command {
-                ClientCommand::ListTargets => crate::client::list_targets(&host, &token).await?,
-                ClientCommand::Run { elf, target } => crate::client::run(&host, &token, &target, &elf).await?,
-            }
-        }
+        Cli::Server { port } => crate::server::serve(port).await,
+        Cli::Client(cmd) => client::main(cmd).await,
     }
-
-    Ok(())
 }
