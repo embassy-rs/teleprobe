c58b4cbe24f8ca2c7a8f0aed21cb8407f4b8cd23
xoviat <xoviat@users.noreply.github.com>
2026-03-14 14:57:07 -0500

    add timeout to local cmd

--- teleprobe/src/main.rs
+++ teleprobe/src/main.rs
@@ -10,7 +10,11 @@ pub mod util;
 
 include!(concat!(env!("OUT_DIR"), "/meta.rs"));
 
+use std::time::{Duration, Instant};
+
 use clap::Parser;
+
+use crate::run::Options;
 
 #[derive(clap::Parser)]
 #[clap(version = "1.0", author = "Dario Nieuwenhuis <dirbaio@dirbaio.net>")]
@@ -31,8 +35,11 @@ enum LocalCommand {
     ListProbes,
     Run {
         /// ELF file to flash+run
-        #[clap(long)]
-        elf: String,
+        file: String,
+
+        /// Set job timeout
+        #[clap(short)]
+        timeout: Option<u64>,
 
         #[clap(flatten)]
         probe: crate::probe::Opts,
@@ -59,11 +66,14 @@ async fn async_main() -> anyhow::Result<()> {
     match cli {
         Cli::Local { command } => match command {
             LocalCommand::ListProbes => crate::probe::list(),
-            LocalCommand::Run { elf, probe } => {
-                let elf = std::fs::read(elf)?;
+            LocalCommand::Run { file, timeout, probe } => {
+                let elf = std::fs::read(file)?;
                 let mut sess = crate::probe::connect(&probe)?;
 
-                let opts = crate::run::Options::default();
+                let opts = Options {
+                    deadline: timeout.map(|t| Instant::now() + Duration::from_secs(t)),
+                    ..Default::default()
+                };
                 crate::run::run(&mut sess, &elf, opts)
             }
         },
